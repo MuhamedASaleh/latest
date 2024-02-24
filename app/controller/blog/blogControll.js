07a48ee6ff8797cfb073e90a8ab9dae4e984f56a
mohamed saleh <mohamed.saleh@code.work>
2024-02-24 14:19:56 +0200

    second"

--- app/controller/blog/blogControll.js
+++ app/controller/blog/blogControll.js
@@ -16,8 +16,10 @@ router.get("/blog", asyncHandler(async (req, res, next) => {
         // });
 
         // const allBlog = await Blog.findAll()
-
-        res.status(200).json({ "blog 1":"lorem is the best title" ,"blog 2":"lorem is the best title" ,"blog 3":"lorem is the best title" });
+        const data = [
+            { "blog 1": "lorem is the best title" }, { "blog 2": "lorem is the best title" }, { "blog 3": "lorem is the best title" }
+        ]
+        res.status(200).json(data);
     } catch (error) {
         res.status(500).json({ error: 'Internal Server Error' });
     }
